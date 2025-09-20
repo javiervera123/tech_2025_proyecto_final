ec397f9707aacd15a3c334b0be7905b9646899d6
javiervera123 <veraadsi123@gmail.com>
2025-09-19 21:59:23 -0500

    Se creó el carrusel en index, se creó las categorías y se creó la página acerca

---
+++ basedatos/datos.js
@@ -0,0 +1,91 @@
+//definimos la lista para alacenar los daros del carrusel
+const elementos_carrusel = [
+    {
+        nombre: "productos de aseo marca ",
+        descripcion: "Producto de aseo",
+        imagen: "image/foto1.jpg"
+    },
+    {
+        nombre: "productos en oferta",
+        descripcion: "Productos con descuentos",
+        imagen: "image/fotopromo.jpg"
+    },
+    {
+        nombre: "Frutas frescas",
+        descripcion: "Las mejores frutas de temporada, directo del campo.",
+        imagen: "image/foto3.jpg"
+    },
+    {
+        nombre: "Verduras saludables",
+        descripcion: "Alimentos verdes para una vida sana.",
+        imagen: "image/foto4.jpg"
+    },
+    {
+        nombre: "Lácteos y quesos",
+        descripcion: "Variedad en leche, yogurt y quesos artesanales.",
+        imagen: "image/foto5.jpg"
+    }
+]
+const lista_categorias =[
+    {   
+    nombre: "Granos y Cereales",
+    descripcion: "Arroz, lentejas, fríjoles, avena, maíz",
+    imagen:"image/categoria1"
+    },
+    {
+    nombre: "Lácteos y Derivados",
+    descripcion: "Leche, yogures, quesos, mantequilla, crema.",
+    imagen:"https://placehold.co/400x400?text=Lácteos"
+    },
+    {   
+    nombre: "Carnes y Pescados",
+    descripcion: "Res, cerdo, pollo, pescado, embutidos.",
+    imagen:"image/categoria1"
+    },
+    {
+    nombre: "Frutas y Verduras",
+    descripcion: "Frescas, congeladas, hierbas y hortalizas.",
+    imagen:"https://placehold.co/400x400?text=Frutas"
+    },
+    {   
+    nombre: "Panadería y Pastelería",
+    descripcion: "Pan, tortas, galletas, arepas",
+    imagen:"image/categoria1"
+    },
+    {
+    nombre: "Bebidas",
+    descripcion: "Gaseosas, jugos, agua, energizantes, café, té.",
+    imagen:"image/categoria1"
+    },
+    {   
+    nombre: "Aseo del Hogar",
+    descripcion: "Detergentes, jabones, limpiadores, desinfectantes",
+    imagen:"image/categoria1"
+    },
+    {
+    nombre: "Cuidado Personal",
+    descripcion: "Shampoo, jabón corporal, cremas, desodorantes.",
+    imagen:"image/categoria1"
+    },
+    {   
+    nombre: "Enlatados y Conservas",
+    descripcion: "Atún, maíz dulce, salsas, sopas, conservas de frutas.",
+    imagen:"image/categoria1"
+    },
+    {
+    nombre: "Aceites y Condimentos",
+    descripcion: "Aceites, sal, azúcar, especias, salsas.",
+    imagen:"image/categoria1"
+    },
+    {   
+    nombre: "Snacks y Golosinas",
+    descripcion: "Papas fritas, chocolates, caramelos, galletas.",
+    imagen:"image/categoria1"
+    },
+    {
+    nombre: "Productos de Limpieza y Desinfección",
+    descripcion: "Aceites, sal, azúcar, especias, salsas.",
+    imagen:"image/categoria1"
+    }
+]
+console.log("✅ datos.js cargado correctamente");
